d11f908df4035cc118f0527161ed3b90cfca42e3
Joane Artiaga <zjnartiaga@globe.com.ph>
2022-08-16 10:08:06 +0800

    unit testing

--- src/test/java/com/example/speech/controller/SpeechControllerTest.java
+++ src/test/java/com/example/speech/controller/SpeechControllerTest.java
@@ -1,4 +1,51 @@
-import static org.junit.jupiter.api.Assertions.*;
-class SpeechControllerTest {
-  
+package com.example.speech.controller;
+
+import com.example.speech.model.Author;
+import com.example.speech.model.Speech;
+import com.example.speech.model.Subject;
+import com.example.speech.service.SpeechService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SpeechControllerTest {
+    @Autowired
+    SpeechService speechService;
+
+    List<Speech> mockSpeeches = new ArrayList<>();
+
+    @Test
+    public void getAllSpeech() {
+        Set<Author> authors = new HashSet<>();
+        authors.add(new Author("Joane", "Artiaga"));
+        authors.add(new Author("Bill", "Pepito"));
+        Set<Subject> subjects = new HashSet<>();
+        subjects.add(new Subject("hello"));
+        subjects.add(new Subject("test"));
+
+        mockSpeeches.add(new Speech(authors,
+            new java.sql.Date(Calendar.getInstance().getTime().getTime()), "test actual speech",
+          subjects));
+
+        authors.add(new Author("Joane", "Artiaga"));
+        authors.add(new Author("Resty", "Artiaga"));
+        subjects.add(new Subject("engaged"));
+        subjects.add(new Subject("congratulations"));
+
+        mockSpeeches.add(new Speech(authors,
+          new java.sql.Date(Calendar.getInstance().getTime().getTime()), "I just got engaged!",
+          subjects));
+
+        Mockito.when(speechService.getAllSpeech()).thenReturn(mockSpeeches);
+        Assert.assertEquals(mockSpeeches.size(),2);
+    }
 }
